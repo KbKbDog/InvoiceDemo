7142a9325f6fcd284cbf2606cbba754bf96bc0d2
KbKbDog <lp.yaoran@foxmail.com>
2023-09-20 22:46:57 +0800

    Changes

--- src/test/java/com/example/invoicedemo/MapperTest.java
+++ src/test/java/com/example/invoicedemo/MapperTest.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 
 @SpringBootTest
-public class MaperTest {
+public class MapperTest {
 
     @Autowired
     private UserRepository userRepository;
